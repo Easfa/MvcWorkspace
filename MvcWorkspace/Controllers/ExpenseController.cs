2d05195d241acfd56d7ec5dc3e73c227c0ad922c
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-16 14:06:03 +0300

    Expense created- delete, index added

---
+++ MvcWorkspace/Controllers/ExpenseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcWorkspace.Data;
+using MvcWorkspace.Models;
+
+namespace MvcWorkspace.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ExpenseController(AppDbContext db) 
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> Expenses = _db.Expenses;
+            return View(Expenses);
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            var expense = _db.Expenses.Find(id);
+            if (expense == null || id == 0) return NotFound();
+            _db.Expenses.Remove(expense);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MvcWorkspace/Data/AppDbContext.cs
+++ MvcWorkspace/Data/AppDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MvcWorkspace.Models;
 
 namespace MvcWorkspace.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options): base(options) { }
+        public AppDbContext(DbContextOptions options) : base(options) { }
 
         //DbSets Area
+
+        public DbSet<Expense> Expenses {get; set;}
     }
 }
