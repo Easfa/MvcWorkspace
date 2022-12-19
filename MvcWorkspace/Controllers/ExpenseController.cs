8ad2547514781cb9eafb895ca29a56e67c6ffab7
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-19 14:08:14 +0300

    EagerLoading added

--- MvcWorkspace/Controllers/ExpenseController.cs
+++ MvcWorkspace/Controllers/ExpenseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MvcWorkspace.Data;
 using MvcWorkspace.Models;
 using MvcWorkspace.Models.ViewModels;
@@ -16,11 +17,14 @@ namespace MvcWorkspace.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Expense> Expenses = _db.Expenses;
-            foreach (var expense in Expenses)
-            { 
-                expense.ExpenseCategory = _db.ExpenseCategories.Find(expense.C_Id); 
-            } 
+            //IEnumerable<Expense> Expenses = _db.Expenses;
+            //foreach (var expense in Expenses)
+            //{
+            //expense.ExpenseCategory = _db.ExpenseCategories.Find(expense.C_Id); 
+            //}
+
+            IEnumerable<Expense> Expenses = _db.Expenses.Include(x => x.ExpenseCategory);
+
             return View(Expenses);
         }
 
